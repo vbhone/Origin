e9ed20171e39e89ba03f11fcfc2a9abbecfc824b
vbhone <1124920238@qq.com>
2019-09-06 15:05:35 +0800

    1.2

---
+++ venv/IdentityDigits.py
@@ -0,0 +1,66 @@
+import os
+from numpy import *
+import operator
+
+def readFile(filename):
+
+    labels=[]
+    index=0
+
+    lists=os.listdir(filename)
+
+    dataSet = zeros((len(lists), 1024))
+    for i in range(len(lists)):
+        path=os.path.join(filename,lists[i])
+        # print(path)
+        # print("\n")
+
+        digitname=path.split('\\')
+        labels.append(digitname[-1][0:1])
+        # print(labels,file=fow)
+        fo=open(path)
+        filecontend=fo.read()
+        filecontend=filecontend.replace("\n","")
+        # print(filecontend)
+        # print("\n")
+        singleData=list(map(int,filecontend))
+        print(i)
+        dataSet[index:]=singleData
+        print(i)
+        index+=1
+    return dataSet,labels
+
+def judge(testDigit,dataSet):
+    dataSize=dataSet.shape[0]
+    testTwo=tile(testDigit,(dataSize,1))
+    testTwo=testTwo-dataSet
+    testTwo=testTwo**2
+    testTwo=testTwo.sum(axis=1)
+    testTwo = testTwo ** 0.5
+    classCount={}
+    testTwo=testTwo.argsort()
+    for i in range(3):
+        voteIlabel=labels[testTwo[i]]
+        classCount[voteIlabel]=classCount.get(voteIlabel,0)+1
+    sortedClassCount=sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
+    print(sortedClassCount[0][0])
+
+
+def getTest(filename,dataSet):
+    lists = os.listdir(filename)
+    # print(len(lists))
+    for i in range(len(lists)):
+        path = os.path.join(filename, lists[i])
+        fo = open(path)
+        filecontend = fo.read()
+        filecontend = filecontend.replace("\n", "")
+        # print(filecontend)
+        singleData = list(map(int,filecontend))
+        judge(singleData,dataSet)
+
+
+
+dataSet,labels=readFile(".\digits\\trainingDigits")
+# print(dataSet)
+testDigits=getTest(".\digits\\testDigits",dataSet)
+
